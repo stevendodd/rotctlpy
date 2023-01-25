b3489bbf6b649d35a67c44fe84c940e9cdbb9368
Steven Dodd <stevendodd@outlook.com>
2023-01-25 20:11:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# hamlib rotctl implementation in python for infrared rotator
+
+* Simple python script listening on port 65432 that accepts rotctl commands
+* Can be used with tools such as [GPredict](http://gpredict.oz9aec.net/) 
+* TV rotators like the [Sharman AR 600](https://moonrakeronline.com/sharman-ar-600-vhf-uhf-antenna-rotator) 
+* Threshold for rotation should be set to > 15 degrees
+
+<img src="./RotatorConfig.png" width=400>
+<p>
+<img src="./Rotator.jpg" width=800>
---
+++ rotctl.py
@@ -0,0 +1,109 @@
+import socket
+import sys
+from datetime import datetime
+
+# The number of degrees of rotation per second
+rot_speed = 4.8
+
+# Assumes rotator has 12 memory buttons labeled A-L
+buttons = ["A","B","C","D","E","F","G","H","I","J","K","L"]
+
+# Assumes rotator button A represents 30°, B 60° etc; 
+# Each setting is made up of (min degrees, max degrees, direction)
+# N Has two list elements as it resets the degrees back to 0
+rot_pos = [
+      ( 15.001, 45, 30),
+      ( 45.001, 75, 60),
+      ( 75.001,105, 90),
+      (105.001,135,120),
+      (135.001,165,150),
+      (165.001,195,180),
+      (195.001,225,210),
+      (225.001,255,240),
+      (255.001,285,270),
+      (285.001,315,300),
+      (315.001,345,330),
+      (345.001,361,  0),
+      (  0,     15,  0)
+      ]
+
+# Initialise rotator
+start_time = datetime.now()
+target_pos = current_pos = start_pos = 0.00
+time = datetime.now().strftime("%H:%M:%S")
+print("[{}] >>>>>> Press button Initial".format(time))
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+server_address = ("192.168.1.10", 65432)
+sock.bind(server_address)
+sock.listen(1)
+
+while True:
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    try:
+        while True:
+            data = connection.recv(16)
+            
+            if data:
+                time = datetime.now().strftime("%H:%M:%S")
+
+                # If we have asked the rotator to move calculate the current position
+                if current_pos != target_pos:
+                    # How far has the rotator moved since we started
+                    t_diff = datetime.now() - start_time
+                    movement = t_diff.seconds * rot_speed
+                    diff = target_pos - start_pos
+                    
+                    # Update the current position
+                    if diff >= 0:
+                        current_pos = round(start_pos + movement, 2)
+                    else:
+                        current_pos = round(start_pos - movement, 2)
+                    
+                    # Has the rotator has arrived at the target position
+                    if abs(target_pos - current_pos) < rot_speed:
+                        current_pos = target_pos
+                        
+                # Read data from antenna control
+                cmd = data.decode("utf-8").strip().split(" ")
+                
+                # Received updated coordinates
+                if cmd[0] == "P":
+                  print("[{}] Setting Az: {} El: {}".format(time,cmd[1],cmd[2]) )
+
+                  for i, p in enumerate(rot_pos):
+                      if p[0] <= float(cmd[1]) <= p[1]:
+                          # Final button N has 2 rot_pos elements
+                          if i == 12: i = 11
+                          print("[{}] >>>>>> Press button {}".format(time,buttons[i]))
+                          
+                          # Start movement countdown
+                          if target_pos != float(p[2]):
+                              start_time = datetime.now() 
+                              start_pos = current_pos
+                              target_pos = float(p[2])
+                              
+                  # Send OK
+                  connection.sendall(b"RPRT 0\n")
+                  
+                # Received get_pos request
+                if cmd[0] == "p":
+                  resp = "{}\n15.000000".format(current_pos)
+                  print("[{}] Sending position: {}".format(time,current_pos))
+                  
+                  # Send current position
+                  connection.sendall(resp.encode('utf-8'))
+                  
+                # Received stop request
+                if cmd[0] == "S":
+                  # Send OK
+                  connection.sendall(b"RPRT 0\n")
+ 
+            else:
+                break
+    finally:
+        connection.close()
